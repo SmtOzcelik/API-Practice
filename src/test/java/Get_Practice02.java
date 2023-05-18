dd3a1351641613dfaa62c5b16f66eb615354d469
samet ozcelik <smtab142790@gmail.com>
2023-05-18 18:11:23 +0300

    18/05/2023

---
+++ src/test/java/Get_Practice02.java
@@ -0,0 +1,27 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class Get_Practice02 {
+   @Test
+    public void get02(){
+       String url = "https://reqres.in/api/users";
+       Response response = given().when().get(url);
+       // given().when().get(url) -> request
+       // Response response -> response
+
+       // Body Test
+        /* id'si 1 olanın datalarınının aşağıdaki gibi olduğunu test ediniz
+             "email": "george.bluth@reqres.in",
+            "first_name": "George",
+            "last_name": "Bluth",
+         */
+
+       // Matcher class ile test
+       response.then().body("data[0].email",equalTo("george.bluth@reqres.in"));
+       response.then().body("data[0].first_name",equalTo("George"));
+       response.then().body("data[0].last_name",equalTo("Bluth"));
+   }
+}
