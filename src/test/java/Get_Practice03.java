783d0790a20a1deecdf1337dd6dc4a64a67823c4
samet ozcelik <smtab142790@gmail.com>
2023-05-19 10:37:24 +0300

    19/05/2023

---
+++ src/test/java/Get_Practice03.java
@@ -0,0 +1,33 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class Get_Practice03 {
+     /*
+      http://dummy.restapiexample.com/api/v1/employees  url'ine
+      GET request'i yolladigimda gelen response'un
+      status kodunun 200 ve content type'inin "application/json"
+      ve employees sayisinin 24
+      ve employee'lerden birinin "Ashton Cox"
+      ve gelen yaslar icinde 21, 61, ve 23 degerlerinden birinin oldugunu test edin.
+ */
+
+    @Test
+    public void get03(){
+        //1-Set the url
+        String url="http://dummy.restapiexample.com/api/v1/employees";
+        //2-Set the expected data
+        //3-Send the Request and get the Response
+        Response response=given().when().get(url);
+        //4-Do Assertions
+        response.then().assertThat().statusCode(200).contentType(ContentType.JSON);
+        response.then().assertThat().body("data",hasSize(24),
+                "data.employee_name",hasItem("Ashton Cox"),"data.employee_age",hasItems(21,61,23));
+
+
+
+    }
+}
