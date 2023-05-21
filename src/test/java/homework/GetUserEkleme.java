1b8926d16f45b375febb63c7eaac8854eda5e809
samet ozcelik <smtab142790@gmail.com>
2023-05-21 12:31:52 +0300

    20/05/2023

--- src/test/java/homework/GetUserEkleme.java
+++ src/test/java/homework/GetUserEkleme.java
@@ -1,13 +1,25 @@
 package homework;
 
-public class GetUserEkleme {
-    //pojo-ObjectMapper ile cözüm
+import Base_Url.PetStoreBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojo.PetStorePojo;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertEquals;
+
+public class GetUserEkleme extends PetStoreBaseUrl {
+    //pojo-Matcher body  ile cözüm
 
     /*
     1-https://petstore.swagger.io/v2/user
+    2-verilen body post yap
+    3-status code 200
     {
   "id": 5,
-  "username": "samet1453",
+  "username": "ahmet1453",
   "firstName": "smt",
   "lastName": "oz",
   "email": "smt@gmail.com",
@@ -15,5 +27,35 @@ public class GetUserEkleme {
   "phone": "123",
   "userStatus": 2
 }
+4-Response data böyle olmali
+{
+    "code": 200,
+    "type": "unknown",
+    "message": "5"
+}
      */
+    @Test
+    public void getuser(){
+        //Set url
+        spec.pathParam("1","user");
+        //Set the expected data
+        PetStorePojo expectedData=new PetStorePojo(5,"ahmet1453","smt","oz","smt@gmail.com","123","123",2);
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request and get the response
+        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{1}");
+        response.prettyPrint();
+        //Do Assertions
+        assertEquals(200,response.statusCode());
+        response.then().assertThat().body("code",equalTo(200)); //body ile assertion
+        assertEquals(response.jsonPath().getInt("code"),200); //jsonPath ile assertion
+
+        response.then().assertThat().body("type",equalTo("unknown"));
+        assertEquals(response.jsonPath().getString("type"),"unknown");
+
+        response.then().assertThat().body("message",equalTo("5"));
+        assertEquals(response.jsonPath().getString("message"),"5");
+
+
+    }
 }
