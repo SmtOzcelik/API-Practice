7f6b3fa5aab4d9bb1cf9405e381a21326d1b58e8
samet ozcelik <smtab142790@gmail.com>
2023-05-21 12:00:46 +0300

    20/05/2023

---
+++ src/test/java/homework/GetUserEkleme.java
@@ -0,0 +1,19 @@
+package homework;
+
+public class GetUserEkleme {
+    //
+
+    /*
+    1-https://petstore.swagger.io/v2/user
+    {
+  "id": 5,
+  "username": "samet1453",
+  "firstName": "smt",
+  "lastName": "oz",
+  "email": "smt@gmail.com",
+  "password": "123",
+  "phone": "123",
+  "userStatus": 2
+}
+     */
+}
